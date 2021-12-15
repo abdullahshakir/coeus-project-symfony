259e37d1b8ecfac3500d461573dd75f8bd15e468
Abdullah <abdullah.shakir1@gmail.com>
2021-12-15 17:01:31 +0500

    update product quantity when buyer place order

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -11,6 +11,7 @@ use App\Form\PlaceOrderType;
 use App\Manager\CartManager;
 use App\Entity\Order;
 use Symfony\Component\Uid\Uuid;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CartController extends AbstractController
 {
@@ -44,7 +45,7 @@ class CartController extends AbstractController
     /**
      * @Route("/cart/order", name="cart_order", methods={"POST"})
      */
-    public function placeOrder(Request $request, CartManager $cartManager): Response
+    public function placeOrder(Request $request, EntityManagerInterface $entityManager, CartManager $cartManager): Response
     {
         $authChecker = $this->get('security.authorization_checker');
         $isRoleBuyer = $authChecker->isGranted('ROLE_BUYER');
@@ -58,6 +59,15 @@ class CartController extends AbstractController
         $cart->setUpdatedAt(new \DateTime());
         $cart->setToken(Uuid::v4());
         $cartManager->save($cart);
+
+
+        $orderProducts = $cart->getOrderProducts();
+        foreach ($orderProducts as $orderProduct) {
+            $product = $orderProduct->getProduct();
+            $product->setQuantity($product->getQuantity() - $orderProduct->getQuantity());
+            $entityManager->persist($product);
+            $entityManager->flush();
+        }
  
         return $this->redirectToRoute('buyer_homepage');
     }
