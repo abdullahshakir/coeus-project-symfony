6fb33db04ea7c44291a95242057451d30a10f838
Abdullah <abdullah.shakir1@gmail.com>
2021-12-17 05:15:45 +0500

    symfony encore used for assets - buyer panel updated

---
+++ assets/scripts/buyer/order_show.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import './jquery-ui.min.js';
+import 'bootstrap';
+import './datatables.bootstrap4.min.js';
+
+$(document).ready(function () {
+    $('#order-products').DataTable();
+});
---
+++ assets/scripts/buyer/product_show.js
@@ -0,0 +1,8 @@
+import '../../styles/buyer/product_show.css';
+import $ from 'jquery';
+import './jquery-ui.min.js';
+import './datatables.bootstrap4.min.js';
+
+$(document).ready(function () {
+    $('#feedbacks').DataTable();
+});
